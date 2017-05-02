c264c0079b38abb57c14f80b2417928009efcdfc
Steve Hu <stevehu@gmail.com>
2017-05-02 19:06:39 -0400

    update docker-entrypoint.sh

--- light-codegen/docker-entrypoint.sh
+++ light-codegen/docker-entrypoint.sh
@@ -6,13 +6,4 @@ codegen="/light-api/light-codegen/codegen-cli/target/codegen-cli.jar"
 
 JAVA_OPTS=${JAVA_OPTS:"-XX:MaxPermSize=256M -Xmx1024M}
 
-case "$1" in
-    generate|help|langs|meta|config-help)
-        command=$1
-        shift
-        exec java ${JAVA_OPTS} -jar ${codegen} ${command} "$@"
-        ;;
-    *)
-         exec "$@"
-        ;;
-esac
+exec java ${JAVA_OPTS} -jar ${codegen} "$@"
