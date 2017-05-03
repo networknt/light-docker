105bbc3fc97035a9dc76657a3fe9cd410615dbdc
Steve Hu <stevehu@gmail.com>
2017-05-03 08:02:30 -0400

    fix a typo in light-codegen

--- light-codegen/docker-entrypoint.sh
+++ light-codegen/docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 codegen="/light-api/light-codegen/codegen-cli/target/codegen-cli.jar"
 
-JAVA_OPTS=${JAVA_OPTS:"-XX:MaxPermSize=256M -Xmx1024M}
+JAVA_OPTS=${JAVA_OPTS:"-XX:MaxPermSize=256M -Xmx1024M"}
 
 exec java ${JAVA_OPTS} -jar ${codegen} "$@"
