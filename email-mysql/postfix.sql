621599e9002a5c142423ef584a5de88481c20d40
Steve Hu <stevehu@gmail.com>
2017-10-09 13:35:40 -0400

    update api compose

--- email-mysql/postfix.sql
+++ email-mysql/postfix.sql
@@ -4,17 +4,6 @@
 -- ------------------------------------------------------
 -- Server version	10.1.16-MariaDB-1~jessie
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `admin`
 --
@@ -24,8 +13,6 @@ GRANT ALL PRIVILEGES ON postfix.* TO 'mysqluser'@'%' WITH GRANT OPTION;
 USE postfix;
 
 DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admin` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -35,16 +22,13 @@ CREATE TABLE `admin` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Virtual Admins';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `admin`
 --
 
 LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
 INSERT INTO `admin` VALUES ('admin@domain.tld','{SHA512-CRYPT}$6$Wt7uQEnB6HPP6mM0$lOP8IKtEUJKWSwczEC5/g6aYamkwh5rx3ztnRuqcRLJjGTXiLpUnxzUgy2rfNieH9C8x7M6Nr9q19SG6njUj//',1,'2016-11-28 08:53:31','2016-11-28 08:53:31',1);
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -52,8 +36,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `alias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `alias` (
   `address` varchar(255) NOT NULL,
   `goto` text NOT NULL,
@@ -64,7 +46,6 @@ CREATE TABLE `alias` (
   PRIMARY KEY (`address`),
   KEY `domain` (`domain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Virtual Aliases';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `alias`
@@ -81,8 +62,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `alias_domain`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `alias_domain` (
   `alias_domain` varchar(255) NOT NULL,
   `target_domain` varchar(255) NOT NULL,
@@ -93,15 +72,12 @@ CREATE TABLE `alias_domain` (
   KEY `active` (`active`),
   KEY `target_domain` (`target_domain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Domain Aliases';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `alias_domain`
 --
 
 LOCK TABLES `alias_domain` WRITE;
-/*!40000 ALTER TABLE `alias_domain` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alias_domain` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -109,8 +85,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `config`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `config` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL DEFAULT '',
@@ -118,16 +92,13 @@ CREATE TABLE `config` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='PostfixAdmin settings';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `config`
 --
 
 LOCK TABLES `config` WRITE;
-/*!40000 ALTER TABLE `config` DISABLE KEYS */;
 INSERT INTO `config` VALUES (1,'version','1835');
-/*!40000 ALTER TABLE `config` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -135,8 +106,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `domain`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `domain` (
   `domain` varchar(255) NOT NULL,
   `description` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -151,16 +120,13 @@ CREATE TABLE `domain` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`domain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Virtual Domains';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `domain`
 --
 
 LOCK TABLES `domain` WRITE;
-/*!40000 ALTER TABLE `domain` DISABLE KEYS */;
 INSERT INTO `domain` VALUES ('ALL','',0,0,0,0,'',0,'2016-11-28 08:53:31','2016-11-28 08:53:31',1),('domain.tld','Test domain',0,0,1,0,'virtual',0,'2016-11-28 08:54:26','2016-11-28 08:54:26',1);
-/*!40000 ALTER TABLE `domain` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -168,8 +134,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `domain_admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `domain_admins` (
   `username` varchar(255) NOT NULL,
   `domain` varchar(255) NOT NULL,
@@ -177,16 +141,13 @@ CREATE TABLE `domain_admins` (
   `active` tinyint(1) NOT NULL DEFAULT '1',
   KEY `username` (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Domain Admins';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `domain_admins`
 --
 
 LOCK TABLES `domain_admins` WRITE;
-/*!40000 ALTER TABLE `domain_admins` DISABLE KEYS */;
 INSERT INTO `domain_admins` VALUES ('admin@domain.tld','ALL','2016-11-28 08:53:31',1);
-/*!40000 ALTER TABLE `domain_admins` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -194,8 +155,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `fetchmail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `fetchmail` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `domain` varchar(255) DEFAULT '',
@@ -222,16 +181,13 @@ CREATE TABLE `fetchmail` (
   `active` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `fetchmail`
 --
 
 LOCK TABLES `fetchmail` WRITE;
-/*!40000 ALTER TABLE `fetchmail` DISABLE KEYS */;
 INSERT INTO `fetchmail` VALUES (1,'domain.tld','sarah.connor@domain.tld','127.0.0.1','password','john.doe@domain.tld','dGVzdHBhc3N3ZDEy','',10,1,1,'IMAP',1,0,'','','','','','2016-12-05 11:59:01','2016-12-05 11:58:53','2016-12-05 11:58:53',1);
-/*!40000 ALTER TABLE `fetchmail` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -239,8 +195,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `log` (
   `timestamp` datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   `username` varchar(255) NOT NULL,
@@ -250,15 +204,12 @@ CREATE TABLE `log` (
   KEY `timestamp` (`timestamp`),
   KEY `domain_timestamp` (`domain`,`timestamp`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Log';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `log`
 --
 
 LOCK TABLES `log` WRITE;
-/*!40000 ALTER TABLE `log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `log` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -266,8 +217,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `mailbox`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mailbox` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -282,16 +231,13 @@ CREATE TABLE `mailbox` (
   PRIMARY KEY (`username`),
   KEY `domain` (`domain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Virtual Mailboxes';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `mailbox`
 --
 
 LOCK TABLES `mailbox` WRITE;
-/*!40000 ALTER TABLE `mailbox` DISABLE KEYS */;
 INSERT INTO `mailbox` VALUES ('john.doe@domain.tld','{SHA512-CRYPT}$6$v1LkarodHyGGmfoy$ZszVBzfEZ0CaVnYaBasgvaHJUCNfxwD/E0eNy3iuix56Vl1ZcuDvG9PVr9JRZx5k.7wp1nMb5M1V4aZXo2yfn0','John DOE','domain.tld/john.doe/',1024000,'john.doe','domain.tld','2016-11-28 08:56:47','2016-11-28 08:56:47',1),('sarah.connor@domain.tld','{SHA512-CRYPT}$6$ub.zCcyeaM7Mhs6S$rL4Yj2.Zsk8aFoF5l1mAddVrPo.UZ/1UrNwBC7UTBrX47cViSHo5eepEes6jMqC21P3cBm82adqJZvo91Ekme0','Sarah CONNOR','domain.tld/sarah.connor/',1024000,'sarah.connor','domain.tld','2016-11-28 08:57:51','2016-11-28 08:57:51',1);
-/*!40000 ALTER TABLE `mailbox` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -299,23 +245,18 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `quota`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quota` (
   `username` varchar(255) NOT NULL,
   `path` varchar(100) NOT NULL,
   `current` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`username`,`path`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `quota`
 --
 
 LOCK TABLES `quota` WRITE;
-/*!40000 ALTER TABLE `quota` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quota` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -323,24 +264,19 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `quota2`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `quota2` (
   `username` varchar(100) NOT NULL,
   `bytes` bigint(20) NOT NULL DEFAULT '0',
   `messages` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `quota2`
 --
 
 LOCK TABLES `quota2` WRITE;
-/*!40000 ALTER TABLE `quota2` DISABLE KEYS */;
 INSERT INTO `quota2` VALUES ('john.doe@domain.tld',0,0),('sarah.connor@domain.tld',0,0);
-/*!40000 ALTER TABLE `quota2` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -348,8 +284,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `vacation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vacation` (
   `email` varchar(255) NOT NULL,
   `subject` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -365,15 +299,12 @@ CREATE TABLE `vacation` (
   PRIMARY KEY (`email`),
   KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Postfix Admin - Virtual Vacation';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `vacation`
 --
 
 LOCK TABLES `vacation` WRITE;
-/*!40000 ALTER TABLE `vacation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vacation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -381,8 +312,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `vacation_notification`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vacation_notification` (
   `on_vacation` varchar(255) CHARACTER SET latin1 NOT NULL,
   `notified` varchar(255) CHARACTER SET latin1 NOT NULL,
@@ -390,28 +319,14 @@ CREATE TABLE `vacation_notification` (
   PRIMARY KEY (`on_vacation`,`notified`),
   CONSTRAINT `vacation_notification_pkey` FOREIGN KEY (`on_vacation`) REFERENCES `vacation` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Postfix Admin - Virtual Vacation Notifications';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `vacation_notification`
 --
 
 LOCK TABLES `vacation_notification` WRITE;
-/*!40000 ALTER TABLE `vacation_notification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vacation_notification` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'postfix'
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-28 10:02:24
