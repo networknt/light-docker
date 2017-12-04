97f77f17fbe81e1c61a5baebc26166315cb816af
Steve Hu <stevehu@gmail.com>
2017-12-03 20:59:57 -0500

    update eventuate

--- eventuate-mysql/mysql-cli.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash -e
-
-docker run $* \
-   --name mysqlterm --link mysql_1:mysql --rm mysql:5.7 \
-   sh -c 'exec mysql -h"$MYSQL_ROOT_PASSWORD" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" -o "$MYSQL_ENV_DATABASE"'
