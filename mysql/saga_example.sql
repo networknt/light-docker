bf17ccb8a5d8aa0e38b18e76cec443c52ac3d2a0
chenyan71 <passWord11!!>
2018-01-19 10:51:35 -0500

    saga customer-order service example ddl change

--- mysql/saga_example.sql
+++ mysql/saga_example.sql
@@ -5,6 +5,7 @@ GRANT ALL PRIVILEGES ON customerorder.* TO 'mysqluser'@'%' WITH GRANT OPTION;
 use customerorder;
 
 DROP table IF EXISTS  customer;
+DROP table IF EXISTS  customer_order;
 DROP table IF EXISTS  order_detail;
 
 
@@ -14,6 +15,13 @@ CREATE  TABLE customer (
   creditLimit numeric
 );
 
+CREATE  TABLE customer_order (
+  customer_id long,
+  order_id long,
+  orderTotal numeric
+);
+
+
 CREATE  TABLE order_detail (
   order_id long ,
   customer_id long,
@@ -21,3 +29,4 @@ CREATE  TABLE order_detail (
   amount numeric
 
 );
+
