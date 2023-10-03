56f4b40cdf5e343296647136624431a653a830e6
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-03 15:48:47 +0300

    fix: correct admin password

--- locustfile.py
+++ locustfile.py
@@ -9,7 +9,7 @@ class OAuthUser(HttpUser):
     def get_access_code(self):
         r = self.client.get(
             "/oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization",
-            auth=('admin', '12345'),
+            auth=('admin', '123456'),
             verify=False,
             allow_redirects=False)
         parsed_redirect = urlparse(r.headers['Location'])
