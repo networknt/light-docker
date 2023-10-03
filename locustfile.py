7c6be53bf362865cc354528a97fb21d80a4df853
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-03 14:40:29 +0300

    add: print response using logging

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task
 
+import logging
 
 class OAuthUser(HttpUser):
     host = "https://localhost:6881/"
@@ -7,4 +8,4 @@ class OAuthUser(HttpUser):
     @task
     def get_access_code(self):
         r = self.client.get("oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization", auth=('admin', '12345'))
-        print(r.text)
+        logging.info(r.text)
