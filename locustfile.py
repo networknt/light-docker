1cbddb3103ec51926e513e07580bd8525b733c94
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-03 15:33:44 +0300

    fix: make valid request

--- locustfile.py
+++ locustfile.py
@@ -6,5 +6,12 @@ import logging
 class OAuthUser(HttpUser):
     @task
     def get_access_code(self):
-        r = self.client.get("/oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization", auth=('admin', '12345'))
-        logging.info(r.text)
+        r = self.client.get(
+            "/oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization",
+            auth=('admin', '12345'),
+            verify=False,
+            allow_redirects=False)
+        if r.status_code == 302:
+            logging.info(r.headers['Location'])
+        else:
+            logging.info("Auth_code endpoint did not redirect")
