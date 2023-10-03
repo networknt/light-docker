61ac5770832fb0dbca2beebe0fe950982291fc29
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2023-10-03 14:35:27 +0300

    fix: declare host attribute for the User

--- locustfile.py
+++ locustfile.py
@@ -2,8 +2,9 @@ from locust import HttpUser, task
 
 
 class OAuthUser(HttpUser):
+    host = "https://localhost:6881/"
 
     @task
     def get_access_code(self):
-        r = self.client.get("https://localhost:6881/oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization", auth=('admin', '12345'))
+        r = self.client.get("oauth2/code?response_type=code&client_id=f7d42348-c647-4efb-a52d-4c5787421e72&redirect_uri=http://localhost:8080/authorization", auth=('admin', '12345'))
         print(r.text)
