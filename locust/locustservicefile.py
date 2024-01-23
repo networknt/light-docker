1134b2d230da45f42387e1fbf18bc9d9b6f69b10
nomiahmed1996 <nomi.ahmed1996@gmail.com>
2024-01-23 11:00:21 +0200

    modified update service

--- locust/locustservicefile.py
+++ locust/locustservicefile.py
@@ -67,7 +67,7 @@ class OAuthServiceRegistration(HttpUser):
         @tag('error', 'register', '400')
         def register_service_400_serviceProfile(self):
             with self.client.post("/oauth2/service", data={
-                "serviceType": "public",
+                "serviceType": "openapi",
                 "serviceProfile": "none",  # Error here
                 "serviceId": str(uuid4())[:8],  # Adjust the serviceId generation as needed
                 "serviceName": str(uuid4())[:32],
@@ -90,7 +90,7 @@ class OAuthServiceRegistration(HttpUser):
         @tag('error', 'register', '404')
         def register_service_404(self):
             with self.client.post("/oauth2/service", data={
-                "serviceType": "public",
+                "serviceType": "openapi",
                 "serviceProfile": "mobile",
                 "serviceId": str(uuid4())[:8],  
                 "serviceName": str(uuid4())[:32],
@@ -121,7 +121,7 @@ class OAuthServiceRegistration(HttpUser):
                 raise RescheduleTask()
             updated_data = {
                 "serviceId": c.serviceId,
-                "serviceType": "public",
+                "serviceType": "swagger",
                 "serviceProfile": "mobile",
                 "serviceName": str(uuid4())[:32],
                 "serviceDesc": str(uuid4()),
@@ -141,3 +141,35 @@ class OAuthServiceRegistration(HttpUser):
                     logging.info(f"Service update failed with unexpected status code: {r.status_code}")
                     r.failure(f"Service update failed with unexpected status code: {r.status_code}")
                 self.interrupt()
+
+        @task(1)
+        @tag('errror','update','404')
+        def update_client_404(self):
+            try:
+                c = SERVICES.pop()
+            except KeyError: #logging.info("No services available to update")
+                raise RescheduleTask()
+            
+            updated_data = {
+                "serviceId" : "",
+                "serviceType" : "swagger",
+                "serviceProfile": "mobile",
+                "serviceName": str(uuid4())[:32],
+                "serviceDesc": str(uuid4()),
+                "scope": "read write",
+                "ownerId" : "admin",
+                "host": "lightapi.net"
+            }
+            
+            with self.client.put("/oauth2/service", json=updated_data, verify=False, allow_redirects=False, catch_response=True) as r:
+                if r.status_code == 404:
+                    logging.info(f"service update without id failed as expected, 404")
+                    SERVICES.add(c)
+                    r.success()
+                else:
+                    SERVICES.add(c)
+                    failstr = str(f"Unexpected status code when updating service without id: {r.status_code}")
+                    logging.info(failstr)
+                    r.failure(failstr)
+            self.interrupt()
+
